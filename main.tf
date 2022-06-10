8bd5f7f83ce2a39fc6db7a5d04741e7ecdae3bcf
sj-williams <steve.williams@digital.justice.gov.uk>
2022-06-10 14:42:12 +0100

    Fix iam user name value for stevewilliams

--- main.tf
+++ main.tf
@@ -124,7 +124,7 @@ module "iam_user_stevewilliams" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-user"
   version = "~> 3.0"
 
-  name                          = "JackStockley"
+  name                          = "SteveWilliams"
   force_destroy                 = true
   create_iam_user_login_profile = false
   create_iam_access_key         = false
